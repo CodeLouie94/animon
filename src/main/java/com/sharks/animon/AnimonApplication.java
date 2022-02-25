b5d90f2d6fb4c1cb488ed17568f3c41a7ce994dc
Llewil Aeneid Montilla <codelouie94@gmail.com>
2022-02-25 11:11:29 -0800

    first commit

---
+++ src/main/java/com/sharks/animon/AnimonApplication.java
@@ -0,0 +1,13 @@
+package com.sharks.animon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnimonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnimonApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sharks/animon/AnimonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sharks.animon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnimonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
